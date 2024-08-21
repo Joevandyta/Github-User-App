14b85b0907e82c6c17cd61dfbf933b9817c941ff
Joevandyta <joevandyta@gmail.com>
2024-08-21 18:47:12 +0800

    add encription

--- core/src/main/java/com/jovan/core/di/NetworkModule.kt
+++ core/src/main/java/com/jovan/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -20,13 +21,25 @@ class NetworkModule {
             level = HttpLoggingInterceptor.Level.BODY
         }
 
+
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "api.github.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname,"sha256/GyhWVHsOXNZc6tGTNd15kXF9YD0kEZaGxYn6MUva5jY=")
+            .add(hostname,"sha256/Wec45nQiFwKvHtuHxSAMGkt19k+uPSw9JlEkxhvYPHk=")
+            .add(hostname,"sha256/i7WTqTvh0OioIruIfFR4kMPnBqrS2rdiVPl/s2uC/CY=")
+            .add(hostname,"sha256/lmo8/KPXoMsxI+J9hY+ibNm2r0IYChmOsF9BxD74PVc=")
+            .add(hostname,"sha256/6YBE8kK4d5J1qu1wEjyoKqzEIvyRY5HyM/NB2wKdcZo=")
+            .add(hostname,"sha256/ICGRfpgmOUXIWcQ/HXPLQTkFPEFPoDyjvH7ohhQpjzs=")
+            .build()
+
         return OkHttpClient.Builder()
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
             .writeTimeout(120, TimeUnit.SECONDS)
             .addInterceptor(loggingInterceptor)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
