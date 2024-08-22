1e929354f0542e0011af1d8d6c987cc9a5166532
Joevandyta <joevandyta@gmail.com>
2024-08-22 10:56:54 +0800

    add encription

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     buildTypes {
         debug {
             buildConfigField("String", "BASE_API", "\"${System.getenv("Base_API") ?: "https://api.github.com/"}\"")
-            buildConfigField("String", "API_KEY", "\"${System.getenv("API_Key") ?: "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"}\"")
+            buildConfigField("String", "API_KEY", "\"${System.getenv("API_Key") ?: "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"}\"")
 
             isMinifyEnabled = true
             proguardFiles(
@@ -37,7 +37,7 @@ android {
         }
         release {
             buildConfigField("String", "BASE_API", "\"${System.getenv("Base_API") ?: "https://api.github.com/"}\"")
-            buildConfigField("String", "API_KEY", "\"${System.getenv("API_Key") ?: "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"}\"")
+            buildConfigField("String", "API_KEY", "\"${System.getenv("API_Key") ?: "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"}\"")
 
             isMinifyEnabled = true
             proguardFiles(
